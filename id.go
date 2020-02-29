95de7d344e21a738f69591084e58c87141f0bb4e
Mehran Prs <mehr.prs@gmail.com>
2020-02-29 16:26:08 +0330

    Chagne ID signatre

--- id.go
+++ id.go
@@ -11,16 +11,18 @@ import (
 // ErrInvalidID is the default invalid id validation error
 var ErrInvalidID = validation.NewError("validation_invalid_id", "ID value is invalid")
 
+type IDValidator func(val interface{}) error
+
 // ID function return new validator to validate id.
-func ID(validationErr error) validation.Rule {
+func ID(idValidator IDValidator) validation.Rule {
 	return validation.By(func(value interface{}) error {
 		value, isNil := validation.Indirect(value)
 		if isNil || validation.IsEmpty(value) {
 			return nil
 		}
 
-		if validationErr != nil {
-			return ErrInvalidID.SetMessage(validationErr.Error())
+		if err := idValidator(value); err != nil {
+			return ErrInvalidID.SetMessage(err.Error())
 		}
 
 		return nil
