51f13b16a05abd1863d5734639535edb6c594723
Mehran Prs <mehr.prs@gmail.com>
2020-04-11 12:58:01 +0430

    Add tel validation

---
+++ tel_test.go
@@ -0,0 +1,36 @@
+package vcomplement
+
+import (
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestIRTel(t *testing.T) {
+	phone := "077-35451354"
+
+	err := validation.Validate(&phone, IRTel)
+	assert.Nil(t, err)
+}
+
+func TestIRTel0(t *testing.T) {
+	phone := "077-35451354"
+
+	err := validation.Validate(&phone, IRTel0)
+	assert.Nil(t, err)
+}
+
+func TestIRTel98(t *testing.T) {
+	phone := "+9877-35451354"
+
+	err := validation.Validate(&phone, IRTel98)
+	assert.Nil(t, err)
+}
+
+func TestInvalidIRTelError(t *testing.T) {
+	phone := "07735451354" // without dash between digits
+
+	err := validation.Validate(&phone, IRTel98)
+
+	assert.Equal(t, ErrIRTelInvalid, err)
+}
