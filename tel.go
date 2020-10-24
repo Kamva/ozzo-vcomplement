3100ef9218767210d3c6288bfe8f87f101302230
Mehran Prs <mehr.prs@gmail.com>
2020-10-24 10:21:08 +0330

    Remove dash for iran tel number

--- tel.go
+++ tel.go
@@ -14,7 +14,7 @@ var ErrIRTelInvalid = validation.NewError("validation_ir_tel_invalid", "Iran tel
 
 // IRTel is the iran telephone number validation rule.
 var (
-	IRTel   = validation.Match(regexp.MustCompile("^(\\+98|0)\\d{2}-\\d{8}$")).ErrorObject(ErrIRTelInvalid)
-	IRTel98 = validation.Match(regexp.MustCompile("^\\+98\\d{2}-\\d{8}$")).ErrorObject(ErrIRTelInvalid)
-	IRTel0  = validation.Match(regexp.MustCompile("^0\\d{2}-\\d{8}$")).ErrorObject(ErrIRTelInvalid)
+	IRTel   = validation.Match(regexp.MustCompile("^(\\+98|0)\\d{10}$")).ErrorObject(ErrIRTelInvalid)
+	IRTel98 = validation.Match(regexp.MustCompile("^\\+98\\d{10}$")).ErrorObject(ErrIRTelInvalid)
+	IRTel0  = validation.Match(regexp.MustCompile("^0\\d{10}$")).ErrorObject(ErrIRTelInvalid)
 )
--- tel_test.go
+++ tel_test.go
@@ -7,28 +7,28 @@ import (
 )
 
 func TestIRTel(t *testing.T) {
-	phone := "077-35451354"
+	phone := "07735451354"
 
 	err := validation.Validate(&phone, IRTel)
 	assert.Nil(t, err)
 }
 
 func TestIRTel0(t *testing.T) {
-	phone := "077-35451354"
+	phone := "07735451354"
 
 	err := validation.Validate(&phone, IRTel0)
 	assert.Nil(t, err)
 }
 
 func TestIRTel98(t *testing.T) {
-	phone := "+9877-35451354"
+	phone := "+987735451354"
 
 	err := validation.Validate(&phone, IRTel98)
 	assert.Nil(t, err)
 }
 
 func TestInvalidIRTelError(t *testing.T) {
-	phone := "07735451354" // without dash between digits
+	phone := "077-35451354" // with dash between digits
 
 	err := validation.Validate(&phone, IRTel98)
 
