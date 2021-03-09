3dff76c6abb27bc683c0c03ee4e0df90862597f3
mehran-prs <mehr.prs@gmail.com>
2021-03-09 12:22:25 +0330

    add Iran IBAN validator

--- financial.go
+++ financial.go
@@ -13,8 +13,10 @@ import (
 // ErrIRCreditCardInvalid is the default IRCreditCard validation rules error.
 var ErrIRCardNumberInvalid = validation.NewError("validation_ir_card_number_invalid", "Iran card number is invalid")
 var ErrIRAccountNumberInvalid = validation.NewError("validation_ir_account_number_invalid", "Iran account number is invalid")
+var ErrIRIBANNumberInvalid = validation.NewError("validation_ir_iban_invalid", "Iran IBAN is invalid")
 
 var (
 	IRCardNumber    = validation.Match(regexp.MustCompile("^\\d{16}$")).ErrorObject(ErrIRCardNumberInvalid)
-	IRAccountNumber = validation.Match(regexp.MustCompile("^(\\d{5}|\\d{16})$")).ErrorObject(ErrIRAccountNumberInvalid)
+	IRAccountNumber = validation.Match(regexp.MustCompile("^(\\d{5,19})$")).ErrorObject(ErrIRAccountNumberInvalid)
+	IRIBAN          = validation.Match(regexp.MustCompile("^IR[0-9]{24}$")).ErrorObject(ErrIRIBANNumberInvalid)
 )
--- financial_test.go
+++ financial_test.go
@@ -24,17 +24,47 @@ func TestInvalidIRPCardNumberError(t *testing.T) {
 }
 
 func TestIRAccountNumber(t *testing.T) {
-	accountNumber1 := "73483"
+	accountNumber1 := "73383"
 	accountNumber2 := "8374837843827383"
+	accountNumber3 := "8374837843827383123" // length 19
 
 	assert.Nil(t, validation.Validate(&accountNumber1, IRAccountNumber))
 	assert.Nil(t, validation.Validate(&accountNumber2, IRAccountNumber))
+	assert.Nil(t, validation.Validate(&accountNumber3, IRAccountNumber))
 }
 
 func TestInvalidIRPAccountNumberError(t *testing.T) {
-	accountNumber := "9361234567"
+	length4 := "9312"
+	length20 := "93129312931293129312"
 
-	err := validation.Validate(&accountNumber, IRAccountNumber)
-
+	err := validation.Validate(&length4, IRAccountNumber)
 	assert.Equal(t, ErrIRAccountNumberInvalid, err)
+
+	err = validation.Validate(&length20, IRAccountNumber)
+	assert.Equal(t, ErrIRAccountNumberInvalid, err)
+}
+
+func TestIRIBAN(t *testing.T) {
+	iban := "IR830120000000000055771565"
+
+	assert.Nil(t, validation.Validate(&iban, IRIBAN))
+}
+
+func TestInvalidIRIBANError(t *testing.T) {
+	length25 := "IR83012000000000055771565"
+	invalidChar := "IR83012000000000055771565d"
+	length27 := "IR8301200000000005577153d53"
+	withoutIR := "US83012000000000055771565"
+
+	err := validation.Validate(&length25, IRIBAN)
+	assert.Equal(t, ErrIRIBANNumberInvalid, err)
+
+	err = validation.Validate(&invalidChar, IRIBAN)
+	assert.Equal(t, ErrIRIBANNumberInvalid, err)
+
+	err = validation.Validate(&length27, IRIBAN)
+	assert.Equal(t, ErrIRIBANNumberInvalid, err)
+
+	err = validation.Validate(&withoutIR, IRIBAN)
+	assert.Equal(t, ErrIRIBANNumberInvalid, err)
 }
